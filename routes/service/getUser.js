32160fa90eab355c45cbc5bd320ae1c7e1861f75
Dash <missionredelephant@gmail.com>
2024-09-25 23:03:52 +0530

    added checkpoint for unverified accounts

--- routes/service/getUser.js
+++ routes/service/getUser.js
@@ -6,11 +6,16 @@ router.post("/", async (req, res) => {
     try {
         const userId = req.user.user_id;
         const accounts_coll = client.db("LinkUp").collection("accounts");
-        const result = await accounts_coll.findOne({ user_id: userId }, { projection: { _id: 0, display_name: 1, username: 1, profile_img: 1, email: 1, bgColor: 1 } });
-        if (result) {
-            return res.status(200).json(result);
+        const result = await accounts_coll.findOne({ user_id: userId }, { projection: { 
+            _id: 0, display_name: 1, username: 1, profile_img: 1, email: 1, bgColor: 1, verified: 1
+        } });
+        if (!result) {
+            return res.status(400).send("Failed to get user");
         }
-        res.status(400).send("Failed to get user");
+        if (!result.verified) {
+            return res.status(400).send("Account is not verified");
+        }
+        return res.status(200).json(result);
     }
     catch (err) {
         console.log(err);
