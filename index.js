ea0699f9e7c11a4c0bf58b7ac0a5f0a82cc35367
Dash <missionredelephant@gmail.com>
2024-09-30 21:29:36 +0530

    now onConnect removes inactive socket ids

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const sizeof = require('object-sizeof');
 
 const jwt = require("jsonwebtoken");
 
+const { black, green, bgGreen, red, bgRed } = require('ansis')
+
 const app = express();
 const httpServer = createServer(app);
 
@@ -34,10 +36,10 @@ client.connect()
 
 const min = 1
 const limiter = rateLimit({
-	windowMs: min * 60 * 1000, // 1 minutes
-	limit: 20, // Limit each IP to x requests per `window` (here, per y minutes)
-	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+    windowMs: min * 60 * 1000, // 1 minutes
+    limit: 20, // Limit each IP to x requests per `window` (here, per y minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
     handler: (req, res, next) => {
         res.status(429).json({ type: "limit", message: "Too many requests. Please try again later" });
     }
@@ -83,10 +85,10 @@ const io = new Server(httpServer, {
     },
 });
 
-io.use(function(socket, next) {
+io.use(function (socket, next) {
     const token = socket.handshake.auth.token;
     if (token) {
-        jwt.verify(token, process.env.TOKEN_KEY, function(err, decoded) {
+        jwt.verify(token, process.env.TOKEN_KEY, function (err, decoded) {
             if (err) {
                 return next(new Error("Invalid token"));
             }
@@ -101,33 +103,52 @@ io.use(function(socket, next) {
 const accounts_coll = client.db("LinkUp").collection("accounts");
 
 async function getBasicUserInfo(user_id) {
-    const result = await accounts_coll.findOne({ user_id }, { projection: { 
-        _id: 0, display_name: 1, username: 1, email: 1
-    } });
+    const result = await accounts_coll.findOne({ user_id }, {
+        projection: {
+            _id: 0, display_name: 1, username: 1, email: 1, socket_ids: 1
+        }
+    });
     return result;
 }
 
-async function updateSocketIdOfUser(method, user_id, socket_id) {
-    if (method === "add") {
-        await accounts_coll.updateOne({ user_id }, { $push: { socket_ids: socket_id } });
-    } else if (method === "remove") {
-        await accounts_coll.updateOne({ user_id }, { $pull: { socket_ids: socket_id } });
-    }
+const addSocketId = async (user_id, socket_id) => {
+    await accounts_coll.updateOne({ user_id }, { $push: { socket_ids: socket_id } });
+}
+
+const removeSocketIds = async (user_id, socket_ids) => {
+    await accounts_coll.updateOne({ user_id }, { $pull: { socket_ids: { $in: [...socket_ids] } } });
+}
+
+async function getInActiveSocketIds(user_socket_ids) {
+    if (!user_socket_ids.length) return [];
+
+    const all_socket_ids = Array.from(await io.allSockets());
+
+    user_socket_ids = user_socket_ids.filter((socket_id) => {
+        return !all_socket_ids.includes(socket_id);
+    })
+
+    return user_socket_ids;
 }
 
 io.on("connection", async (socket) => {
     const basicUserInfo = await getBasicUserInfo(socket.user.user_id);
-    await updateSocketIdOfUser('add', socket.user.user_id, socket.id);
-    console.log("a user connected");
-    console.table({...basicUserInfo, socket_id: socket.id});
 
+    try {
+        const filtered_socket_ids = await getInActiveSocketIds(basicUserInfo.socket_ids);
+        if (filtered_socket_ids) {
+            await removeSocketIds(socket.user.user_id, filtered_socket_ids);
+        }
+    } catch (err) {}
+
+    await addSocketId(socket.user.user_id, socket.id);
+    console.log(black.bgGreen(basicUserInfo.username));
+    // const all_sockets = await io.allSockets();
 
     socket.on("disconnect", async (reason) => {
-        await updateSocketIdOfUser('remove', socket.user.user_id, socket.id);
-        console.log("a user disconnected");
-        console.table({...basicUserInfo, socket_id: socket.id});
-        console.log(reason);
-        accounts_coll.updateOne({ user_id: socket.user.user_id }, { $set: { last_seen: new Date() }});
+        await removeSocketIds(socket.user.user_id, [socket.id]);
+        console.log(black.bgRed(basicUserInfo.username));
+        accounts_coll.updateOne({ user_id: socket.user.user_id }, { $set: { last_seen: new Date() } });
     });
 });
 
